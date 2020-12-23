617e33ca276836c1400ff43f29be9cd04c47fef3
unknown <moody.kh.022@gmail.com>
2020-12-23 14:40:10 +0000

    kmom03

--- view/weather/index.php
+++ view/weather/index.php
@@ -6,11 +6,11 @@
 <h1>Hej</h1>
 
 
-<h3>To get the forecast weather for 5 days a head </h3>
+<h3>To get the forecast weather for 10 days a head </h3>
 <div>
-<a href="weather_after"><button  style="padding:2%; font-family: Times New Roman, Times, serif; font-size: 1.1em; border-radius: 25px; " class="pl1"> 5 Days</button></a>
+<a href="weather_after"><button  style="padding:2%; font-family: Times New Roman, Times, serif; font-size: 1.1em; border-radius: 25px; " class="pl1"> 10 Days</button></a>
 </div>
 
-<h3>To get the weather forecast ten days ago</h3>
-<a style="" href="weather_befor"><button style="padding:2%; font-family: Times New Roman, Times, serif; font-size: 1.1em; border-radius: 25px; class="pl1"> 10 Days</button></a>
+<h3>To get the weather forecast 5 days ago</h3>
+<a style="" href="weather_befor"><button style="padding:2%; font-family: Times New Roman, Times, serif; font-size: 1.1em; border-radius: 25px; class="pl1"> 5 Days</button></a>
        
