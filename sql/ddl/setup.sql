2da4aa84cd2dbd572cd0550ab46068b56c6557f9
unknown <moody.kh.022@gmail.com>
2021-01-05 16:15:32 +0000

    kmom03_tag1

---
+++ sql/ddl/setup.sql
@@ -0,0 +1,11 @@
+DROP DATABASE library;
+CREATE DATABASE IF NOT EXISTS library;
+
+CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY '1234';
+
+USE library;
+
+
+
+
+-- GRANT ALL PRIVILEGES ON library.* TO 'user'@'localhost';
